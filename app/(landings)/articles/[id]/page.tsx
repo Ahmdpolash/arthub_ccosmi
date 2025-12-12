b3e88bd8277129d4eeaa3117133d9e0ca2b17fbb
ahmdpolash <ahmedpolash732@gmail.com>
2025-12-12 15:43:18 +0600

    add article details and pagination components

---
+++ app/(landings)/articles/[id]/page.tsx
@@ -0,0 +1,152 @@
+import { BookOpenText } from "lucide-react";
+import Container from "@/components/shared/Container";
+import Image from "next/image";
+
+const ArticleDetailsPage = () => {
+  const isPremium = true;
+  const isPaid = false;
+
+  const articleData = {
+    title:
+      "The Rise of Digital Expression: How Technology Is Redefining Contemporary Art",
+    status: "Paid",
+    postedDate: "Posted 18 Sept 2025",
+  };
+
+  return (
+    <Container>
+      {/* Hero Image */}
+      <div className="mb-6">
+        <img
+          src="/image.png"
+          alt="Digital Art"
+          className="w-full h-96  object-cover rounded-2xl aspect-video"
+        />
+      </div>
+
+      {/* Title */}
+      <div className="mb-6">
+        <h1
+          className="text-xl md:text-2xl lg:text-3xl 
+        text-center lg:text-left font-bold text-gray-900"
+        >
+          {articleData.title}
+        </h1>
+      </div>
+      <div className="relative bg-white rounded-lg overflow-hidden">
+        <div
+          className={`p-2 lg:p-0 md:p-3 ${
+            isPremium && !isPaid ? " pointer-events-none" : ""
+          }`}
+        >
+          <div className="flex items-center lg:justify-end justify-start gap-3 mb-6">
+            <span className="px-3 py-1 rounded-md text-sm font-medium bg-purple-50 text-purple-700 border border-purple-200">
+              {isPremium && !isPaid ? "Paid" : "free"}
+            </span>
+            <span className="text-sm text-gray-500">
+              {articleData.postedDate}
+            </span>
+          </div>
+          <div className="mb-6">
+            <h2 className="text-base font-bold text-gray-900 flex items-center gap-2">
+              <span className="text-black">
+                <BookOpenText />
+              </span>
+              Body (Detailed Content):
+            </h2>
+          </div>
+          <div className="mb-6">
+            <h3 className="text-base font-bold text-gray-900 mb-3">
+              Introduction
+            </h3>
+            <p className="text-sm text-gray-700 leading-relaxed mb-4">
+              Over the last decade, the art world has witnessed a revolutionary
+              shift — from physical canvases to digital screens. Artists today
+              aren't just painters or sculptors: they're coders, 3D designers,
+              and AI collaborators. The rise of technology has given birth to an
+              entirely new creative movement — Digital Art.
+            </p>
+            <p className="text-sm text-gray-700 leading-relaxed mb-4">
+              Over the last decade, the art world has witnessed a revolutionary
+              shift — from physical canvases to digital screens. Artists today
+              aren't just painters or sculptors: they're coders, 3D designers,
+              and AI collaborators. The rise of technology has given birth to an
+              entirely new creative movement — Digital Art. Over the last
+              decade, the art world has witnessed a revolutionary shift — from
+              physical canvases to digital screens. Artists today aren't just
+              painters or sculptors: they're coders, 3D designers, and AI
+              collaborators. The rise of technology has given birth to an
+              entirely new creative movement — Digital Art.
+            </p>
+            <p className="text-sm text-gray-700 leading-relaxed mb-4">
+              Over the last decade, the art world has witnessed a revolutionary
+              shift — from physical canvases to digital screens. Artists today
+              aren't just painters or sculptors: they're coders, 3D designers,
+              and AI collaborators. The rise of technology has given birth to an
+              entirely new creative movement — Digital Art.
+            </p>
+          </div>
+
+          {/* The Evolution of Digital Tools */}
+          <div className="mb-6">
+            <h3 className="text-base font-bold text-gray-900 mb-3">
+              The Evolution of Digital Tools
+            </h3>
+            <p className="text-sm text-gray-700 leading-relaxed mb-4">
+              It all began with simple tools like Adobe Photoshop and CoreDRAW,
+              but now artists are using AI tools such as Midjourney, DAL·E, and
+              Procreate to create masterpieces that blend human imagination with
+              machine intelligence.
+            </p>
+            <p className="text-sm text-gray-700 leading-relaxed mb-4">
+              Platforms like Instagram and Bechance have also made it easier for
+              digital artists to reach global audiences instantly.
+            </p>
+          </div>
+
+          {/* NFTs and the Blockchain Revolution */}
+          <div className="mb-6">
+            <h3 className="text-base font-bold text-gray-900 mb-3">
+              $NFTs and the Blockchain Revolution
+            </h3>
+            <p className="text-sm text-gray-700 leading-relaxed mb-4">
+              In 2021, NFTs (Non-Fungible Tokens) exploded onto the scene.
+              Suddenly, digital art could be owned, traded, and valued — just
+              like traditional paintings.
+            </p>
+          </div>
+        </div>
+
+        {/* Premium Overlay And Bottom Section */}
+        {isPremium && !isPaid && (
+          <div className="absolute w-full bottom-0 h-3/4 bg-gradient-to-b from-transparent via-white to-white flex items-end lg:items-center md:items-center justify-center lg:justify-center  rounded-b-lg">
+            <div className="text-center py-16 px-8">
+              <div className="flex justify-center mb-6">
+                <div className="w-24 h-24 bg-white rounded-full flex items-center justify-center">
+                  {/* <Lock className="w-12 h-12 text-gray-900" /> */}
+                  <Image
+                    src={"/Frame.svg"}
+                    width={160}
+                    height={180}
+                    alt="lockIcon"
+                  />
+                </div>
+              </div>
+              <h2 className="text-4xl font-bold text-gray-900 leading-tight">
+                This article is for premium
+              </h2>
+              <p className="text-4xl font-bold text-gray-900 mb-8">
+                members only.
+              </p>
+              <button className="px-8 py-3 cursor-pointer bg-gray-900 text-white rounded-lg text-base font-medium hover:bg-gray-800 transition-colors">
+                Become a Member
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default ArticleDetailsPage;
---
+++ app/(landings)/articles/page.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import Container from "@/components/shared/Container";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import React, { useState } from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { cards } from "@/constant/dummy";
+import Link from "next/link";
+
+const ArticlesPage = () => {
+  const [page, setPage] = useState(1);
+  const totalPages = 8; // change according to your data
+  return (
+    <div className="bg-grayBg">
+      <Container>
+        <div className="space-y-2">
+          <h1 className="text-2xl lg:text-4xl font-semibold lg:font-bold">
+            Article
+          </h1>
+          <p className="text-[18px]">
+            Featuring the journeys and ideas of artists from around the world.
+          </p>
+        </div>
+
+        <div className="w-full my-3">
+          <div className="relative flex-1 w-full">
+            <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
+            <Input
+              placeholder="Search Article by name"
+              className="pl-10 h-11 rounded-lg border-gray-300 bg-white"
+            />
+          </div>
+        </div>
+
+        <div className=" mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
+          {cards.map((c, idx) => (
+            <Link href={`/articles/${3}`} key={idx}>
+              <article className="relative flex flex-col lg:flex-row items-start bg-white rounded-2xl p-6 border">
+                <div className="flex-1 pr-4 order-2 lg:order-1 ">
+                  <div className="flex items-center gap-3 mb-2 lg:mb-4 mt-4 lg:mt-0">
+                    <div className="w-8 h-8 rounded-full bg-emerald-800 flex items-center justify-center text-white text-sm font-semibold">
+                      CC
+                    </div>
+                    <span className="text-sm text-gray-600">{c.site}</span>
+                  </div>
+
+                  <h3 className="text-lg md:text-2xl font-semibold text-gray-800 leading-tight mb-3">
+                    {c.title}
+                  </h3>
+
+                  <p className="text-sm text-gray-500 max-w-prose">
+                    {c.excerpt}
+                  </p>
+                </div>
+
+                {/* right image */}
+                <div className="flex-shrink-0 lg:ml-4 order-1 lg:order-2">
+                  <div className="w-full h-24 lg:w-28 lg:h-28 rounded-lg overflow-hidden ring-0">
+                    {/* Use next/image or img depending on your stack */}
+                    <img
+                      src={"/lemon.png"}
+                      alt="card"
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                </div>
+              </article>
+            </Link>
+          ))}
+        </div>
+
+        {/* pagination */}
+        <div className="flex justify-center mt-12">
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (page > 1) setPage(page - 1);
+                  }}
+                />
+              </PaginationItem>
+
+              <PaginationItem>
+                <PaginationLink
+                  href="#"
+                  isActive={page === 1}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setPage(1);
+                  }}
+                >
+                  1
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setPage(2);
+                  }}
+                >
+                  2
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setPage(3);
+                  }}
+                >
+                  3
+                </PaginationLink>
+              </PaginationItem>
+
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+
+              <PaginationItem>
+                <PaginationLink
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setPage(totalPages);
+                  }}
+                >
+                  {totalPages}
+                </PaginationLink>
+              </PaginationItem>
+
+              <PaginationItem>
+                <PaginationNext
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (page < totalPages) setPage(page + 1);
+                  }}
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ArticlesPage;
